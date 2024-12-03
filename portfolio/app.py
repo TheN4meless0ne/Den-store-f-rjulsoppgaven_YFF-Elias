ddfcafe08b0f027851f352e68c62f45a1057f90e
TheN4meless0ne <thenmelessne@gmail.com>
2024-12-03 10:07:26 +0100

    added cv

--- portfolio/app.py
+++ portfolio/app.py
@@ -11,5 +11,9 @@ app.secret_key = os.getenv('SECRET_KEY')
 def index():
     return render_template('index.html')
 
+@app.route('/cv')
+def cv():
+    return render_template('page/cv.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
