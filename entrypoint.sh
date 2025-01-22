bcbf8c39ba756e9c22e4d6f6d972bcb3a17e6367
TheN4meless0ne <thenmelessne@gmail.com>
2025-01-22 10:41:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
---mount=type=ssh git clone --recurse-submodules git@github.com:TheN4meless0ne/Den-store-f-rjulsoppgaven_YFF-Elias .
+git clone --recurse-submodules git@github.com:TheN4meless0ne/Den-store-f-rjulsoppgaven_YFF-Elias .
 
 flask run --host=0.0.0.0
